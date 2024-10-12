030eabab80ffb94338567ed85fa59ef8b25415e8
kmate <kmate.dev@gmail.com>
2024-10-12 21:17:25 +0200

    Init

---
+++ PAM_iOS/PAM_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAM_iOSApp.swift
+//  PAM_iOS
+//
+//  Created by kmate on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PAM_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
