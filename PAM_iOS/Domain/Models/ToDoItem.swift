3386ca64c3b645bd588810106facd631b519b1a5
kmate <kmate.dev@gmail.com>
2024-10-13 09:17:39 +0200

    Added model class

---
+++ PAM_iOS/Domain/Models/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  PAM_iOS
+//
+//  Created by kmate on 13/10/2024.
+//
+
+import Foundation
+
+struct ToDoItem: Identifiable {
+    let id: Int
+    let name: String
+    let completed: Bool
+}
