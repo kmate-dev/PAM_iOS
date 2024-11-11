e1c9f96a216b4a57496343a6ebf83edf6af33746
kmate <kmate.dev@gmail.com>
2024-11-11 09:24:39 +0100

    Implemented LocalDataStore using UserDefaults

--- PAM_iOS/DI/LocalDataStore.swift
+++ PAM_iOS/DI/LocalDataStore.swift
@@ -12,3 +12,32 @@ protocol LocalDataStore {
     func getToDoItems() -> [ToDoItem]
     func getToDoItem(itemId: Int) -> ToDoItem?
 }
+
+class LocalDataStoreImpl: LocalDataStore {
+    private let toDoItemsListKey = "TODO_ITEMS_KEY"
+    
+    private let userDefaults: UserDefaults
+    
+    init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+    }
+    
+    func getToDoItems() -> [ToDoItem] {
+        if let savedToDoItemsString = userDefaults.data(forKey: toDoItemsListKey),
+           let savedToDoItems = try? JSONDecoder().decode([ToDoItem].self, from: savedToDoItemsString) {
+            return savedToDoItems
+        }
+        return []
+    }
+    
+    func updateToDoItems(items: [ToDoItem]) {
+        if let encodedItems = try? JSONEncoder().encode(items) {
+            userDefaults.set(encodedItems, forKey: toDoItemsListKey)
+        }
+    }
+    
+    func getToDoItem(itemId: Int) -> ToDoItem? {
+        let toDoItems = getToDoItems()
+        return toDoItems.first(where: { $0.id == itemId })
+    }
+}
