96126c2c063d3aa075e3c8dc63266bd37b8b7364
kmate <kmate.dev@gmail.com>
2024-12-07 18:24:52 +0100

    Added Basic Unit test

---
+++ PAM_iOSTests/BookStoreTest.swift
@@ -0,0 +1,51 @@
+//
+//  BookStoreTest.swift
+//  PAM_iOSTests
+//
+//  Created by kmate on 07/12/2024.
+//
+
+import XCTest
+@testable import PAM_iOS
+
+final class BookStoreTest: XCTestCase {
+    private var store = BookStore(initialBooks: [])
+    private let book1 = Book(
+        id: 0,
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+        price: 20.0
+    )
+    private let book2 = Book(
+        id: 1,
+        title: "Harry Potter 2",
+        author: "J.K. Rowling",
+        price: 30.0
+    )
+    private let book3 = Book(
+        id: 2,
+        title: "Harry Potter 3",
+        author: "J.K. Rowling",
+        price: 50.0
+    )
+    
+
+    override func setUpWithError() throws {
+        store = BookStore(initialBooks: [book1, book2, book3])
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testRemoveBookRemovesBookWithMatchingId() throws {
+        //SetUp
+        let expectedBooks = [book2, book3]
+        
+        //Actions
+        store.removeBook(byId: 0)
+        
+        //Verification
+        XCTAssertEqual(expectedBooks, store.getAllBooks())
+    }
+}
